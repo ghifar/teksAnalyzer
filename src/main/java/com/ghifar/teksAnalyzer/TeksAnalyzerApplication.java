8db5b60bba71bd7de00e204f13612413e753fe60
abudzar al ghiffari <zottagifar@gmail.com>
2019-10-23 22:51:01 +0700

    push awal

---
+++ src/main/java/com/ghifar/teksAnalyzer/TeksAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ghifar.teksAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeksAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeksAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghifar/teksAnalyzer/TeksAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghifar.teksAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeksAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
